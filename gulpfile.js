a4e190afe02af99770f4a5959cfb70dab6268aef
Vadim Rulev <rulevadim@yandex.ru>
2018-12-16 21:15:56 +0300

    Добавил поддержку спрайтов

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,21 @@ gulp.task('sass', function(){
 		.pipe(gulp.dest('web/css'));
 });
 
+gulp.task('sprite', function() {
+	var spriteData = 
+		gulp.src('dev/sprite/*.png')
+			.pipe(spritesmith({
+				imgName: 'sprite.png',
+				cssName: '_sprite.sass',
+				cssTemplate: 'sass.sprite.output.options'
+			}));
+	var imgStream = spriteData.img
+		.pipe(gulp.dest('web/css'));
+	var cssStream = spriteData.css
+		.pipe(gulp.dest('dev/sass'));
+	return merge(imgStream, cssStream);
+});
+
 gulp.task('watch', function(){
 	gulp.watch('dev/sass/*.sass', gulp.series('sass'));
 });
@@ -19,4 +34,4 @@ gulp.task('server', function(){
 	browserSync.watch('web/**/*.{html,js,css}').on('change', browserSync.reload);
 });
 
-gulp.task('default', gulp.series('sass', gulp.parallel('watch', 'server')));
+gulp.task('default', gulp.series('sprite', 'sass', gulp.parallel('watch', 'server')));
