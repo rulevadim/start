e52e8b55a8e0373aae7e2d17a4fb435652557106
Rulev Vadim <rulevadim@yandex.ru>
2019-01-24 15:36:14 +0300

    Add new gulpfile.js package.json

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,26 @@ const sass        = require('gulp-sass');
 const bs          = require('browser-sync');
 const spritesmith = require('gulp.spritesmith');
 const merge       = require('merge-stream');
+const gcmq        = require('gulp-group-css-media-queries');
+const newer       = require('gulp-newer');
+const autoprefix  = require('gulp-autoprefixer');
+
+let site = ''
+
+let proxy = 'http://' + site + '/';
+let webPath = 'D:/OpenServer/OSPanel/domains/' + site;
+let path = {
+	sass: 'dev/sass',
+	css: webPath + '/css',
+	web: 'web'
+};
 
 gulp.task('sass', function(){
-	return gulp.src('dev/sass/*.sass')
+	return gulp.src(path.sass + '/*.sass')
 		.pipe(sass())
-		.pipe(gulp.dest('web/css'));
+		.pipe(autoprefix())
+		.pipe(gcmq())
+		.pipe(gulp.dest(path.css));
 });
 
 gulp.task('sprite', function() {
@@ -27,14 +42,26 @@ gulp.task('sprite', function() {
 	return merge(imgStream, cssStream);
 });
 
+gulp.task('toserver', function(){
+	return gulp.src(path.web + '/**/*.*')
+		.pipe(newer(webPath))
+		.pipe(gulp.dest(webPath));
+});
+
 gulp.task('watch', function(){
-	gulp.watch('dev/sass/*.sass', gulp.series('sass'));
+	gulp.watch(path.web + '/**/*.*', gulp.series('toserver'));
+	gulp.watch(path.sass + '/*.sass', gulp.series('sass'));
 	gulp.watch('dev/sprite/*.{png,jpg,jpeg}', gulp.series('sprite'));
 });
 
 gulp.task('server', function(){
-	bs.init({server:'web'});
-	bs.watch('web/**/*.{html,js,css}').on('change', bs.reload);
+	if (proxy) {
+		bs.init({proxy: proxy});
+	} else {
+		bs.init({server: webPath});
+	}
+
+	bs.watch(webPath + '/**/*.{php,html,js,css}').on('change', bs.reload);
 });
 
-gulp.task('default', gulp.series('sprite', 'sass', gulp.parallel('watch', 'server')));
+gulp.task('default', gulp.series('toserver', 'sprite', 'sass', gulp.parallel('watch', 'server')));
